0d038342865438885e821b56fe77c85fc52b396e
Lenny <55984010+LennyMueller@users.noreply.github.com>
2021-01-11 00:33:33 +0100

    Add files via upload

--- src/script/lib.js
+++ src/script/lib.js
@@ -1,3 +1,11 @@
-function test() {
-  console.log("test");
-}
+$('.navbar a').on('click', function(e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800);
+  }
+});
